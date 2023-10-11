cf96269bde30ced1ad291660e92f96f2dcb29bd3
Noah Willis <noahwillis@Noahs-MacBook-Air.local>
2023-10-10 22:05:12 -0700

    Created the files for the routes and boiler code

---
+++ bueche-business-coaching/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import About from "./routes/about";
+// import Signup from "./routes/signup";
+// import Login from "./routes/login";
+// import Navbar from "./routes/navbar";
+// import Services from "./routes/services";
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+  return 
+
+  // <Navbar/> 
+  // <About/> 
+  // <Login/>
+  // <Signup/>
+  // <Services/>
+  // <Routes>
+  //   <Route>
+
+  //   </Route>
+  // </Routes>
+
+}
+
+export default App;
